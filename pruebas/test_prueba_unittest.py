2948ad3606c20cc8f6af6cba3d5000e5046bc39c
José Hernández <carrousel.182@hotmail.com>
2024-06-23 20:33:37 -0600

    Sesión 3 testingo con pytest

---
+++ pruebas/test_prueba_unittest.py
@@ -0,0 +1,24 @@
+import unittest
+
+class TestPrueba(unittest.TestCase):
+
+    #1 - Arrange o setup
+    def setUp(self) -> None:
+        print("""
+        #1 - Configurando el entorno de los test
+        - Se creo el carrito de compras vacío - OK
+        """)
+
+    def test_agregar_producto(self):
+        print("""
+        #2 - Código necesario para agregar el producto al carrito de compras
+        """)
+        print("### añadiendo el producto")
+        assert False #3 - Assert o evaluación
+        print("### Añadido con éxito - OK")
+
+    # 4 - Cleanup o limpieza
+    def tearDown(self):
+        print("""
+        #4 - Se estan eliminando todos los productos del carrito
+        """)
---
+++ pruebas/test_sql.py
@@ -0,0 +1,32 @@
+import sqlite3
+import unittest
+
+
+class TestSQL(unittest.TestCase):
+
+    def setUp(self):
+        self.db = sqlite3.connect("./test.db")
+        self.cursor = self.db.cursor()
+        self.cursor.execute("DELETE FROM persona")
+        self.db.commit()
+
+    def test_crear_persona(self):
+        # region -- Creando a la persona --
+        self.cursor.execute("INSERT INTO persona (nombre, apellido, id) VALUES (?, ?, ?)", ("José", "Hernández", 1))
+        self.db.commit()
+        # endregion
+
+        # region -- Assert --
+        persona = self.cursor.execute("SELECT * FROM persona WHERE persona.id=1").fetchone()
+        print(persona)
+        assert persona is not None
+        self.db.commit()
+        # endregion
+        # Tambien mse puede hacer el clan up en cada función de prueba
+
+    def tearDown(self):
+        # Limpiar base de datos
+        self.cursor.execute("DELETE FROM persona")
+        self.db.commit()
+        self.cursor.close()
+        self.db.close()
